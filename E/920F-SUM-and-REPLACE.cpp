d536f15a9d095ff660670e25695f5fb3ed94c051
njuapp <854803517@qq.com>
2018-04-12 23:44:57 +0800

    Apr 13rd's task

---
+++ E/920F-SUM-and-REPLACE.cpp
@@ -0,0 +1,10 @@
+//Author: Cheng Hang(calvinhance@gmail.com)
+//Date: 2018-04-12 23:39:54
+//Last modified: 2018-04-12 23:40:22
+#include <iostream>
+#include <vector>
+using namespace std;
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+int main(){
+    return 0;
+}
