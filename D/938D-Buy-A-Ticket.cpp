8a6def1fa46cf88411912f1b480b7bcd770b7b9f
njuapp <854803517@qq.com>
2018-04-21 09:48:34 +0800

    Dijkstra Algorithm

--- D/938D-Buy-A-Ticket.cpp
+++ D/938D-Buy-A-Ticket.cpp
@@ -1,9 +1,53 @@
-//Author: Cheng Hang(calvinhance@gmail.com)
-//Date: 2018-02-22 15:52:29
-//Last modified: 2018-02-22 17:13:13
+/*
+ * Author: Cheng Hang on 04/10/2018
+ * All Copyrights Reserved.
+ */
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <queue>
 using namespace std;
-#define forn(i, n) for (int i = 0; i < int(n); i++)
+typedef long long int ll;
+typedef pair<ll,int> P;
+#define MAX 200010
+vector<P> G[MAX];
+ll dist[MAX];
+set<P> s;
+using namespace std;
 int main(){
-    
+    int n, m;
+    scanf("%d %d", &n, &m);
+    while(m--){
+        int s,j;
+        ll w;
+        scanf("%d %d %lld", &s, &j, &w);
+        G[s].push_back(make_pair(2*w,j));
+        G[j].push_back(make_pair(2*w,s));
+    }
+    for(int i = 1; i <= n; i++){
+        ll p;
+        scanf("%lld", &p);
+        G[0].push_back(make_pair(p, i));
+        dist[i] = p + 1;
+    }
+    dist[0] = 0;
+    s.insert(P(0,0));
+    while(s.size()){
+        auto node = *s.begin();
+        s.erase(s.begin());
+        for(auto e: G[node.second]){
+            if(dist[e.second] > dist[node.second] + e.first){
+                s.erase(P(dist[e.second], e.second));
+                dist[e.second] = e.first + dist[node.second];
+                s.insert(P(dist[e.second], e.second));
+            }
+        }
+    }
+    for(int i = 1; i <= n; i++)
+        printf("%lld ", dist[i]);
+    return 0;
+}
