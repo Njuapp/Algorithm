332eef8130b33c133af2c87f3283353af2587e73
njuapp <854803517@qq.com>
2018-08-06 22:59:49 +0800

    game + cached dp

--- D/918D.cpp
+++ D/918D.cpp
@@ -10,34 +10,35 @@ using namespace std;
 typedef long long int LL;
 typedef pair<int,int> pii;
 #define rep(i,n) for(int i = 0; i < (n); i ++)
-string s;
+int n, m;
+int w[105][105];
+int dp[105][105][30];
+int dfs(int i, int j, int prev){
+    if(dp[i][j][prev]) return dp[i][j][prev];
+    for(int k = 1; k <= n; k ++){
+        if(k == i) continue;
+        if(w[i][k] - 'a' >= prev && dfs(j, k, w[i][k] - 'a') == 2)
+            return dp[i][j][w[i][k] - 'a'] = 1;
+    }
+    return dp[i][j][prev] = 2;
+}
 int main(int argc, const char * argv[]) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    cin >> s;
-    int ans = 0;
-    for(int i = 0; i < s.length(); i ++){
-        int low = 0, high = 0;
-        for(int j = i; j < s.length(); j ++){
-            if(s[j] == '(') {
-                low ++;
-                high ++;
-            }
-            else if(s[j] == ')'){
-                low --;
-                high --;
-            }
-            else{
-                low --;
-                high ++;
-            }
-            if(high < 0) break;
-            if(low < 0) low = 0;
-            if(((j-i) & 1)&&low == 0)
-                ans ++;
-        }
+    cin >> n >> m;
+    while(m--){
+        int u, v;
+        string ww;
+        cin >> u >> v >> ww;
+        w[u][v] = ww[0];
     }
-    cout<<ans<<endl;
+    for(int i = 1; i <= n; i ++){
+        for(int j = 1; j <= n; j ++){
+            if(dfs(i,j,0) == 1)cout<<'A';
+            else cout<<'B';
+        }
+        cout<<endl;
+    }
     return 0;
 }
